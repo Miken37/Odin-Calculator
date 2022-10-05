df8709904cc2fa0cc40a654a03fc04f2ab5df27b
Miken37 <mike-166@hotmail.com>
2022-10-05 06:25:05 -0400

    Added in html and css for the calculator design

---
+++ code.js
@@ -0,0 +1,35 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operation, num1, num2){
+    switch(operation){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1,num2);
+        default:
+            console.log("Invalid");
+    }
+}
+
+let screenDisplay = document.querySelector(".screen");
+let numButOne = document.querySelector(".numOne");
+numButOne.addEventListener("click", () => console.log("worked"));
+
